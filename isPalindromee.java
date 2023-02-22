5477c6142bc8e73aec0b7319586bf6221b035579
xParadize <73064983+xParadize@users.noreply.github.com>
2023-02-22 19:52:54 +0300

    Add files via upload

---
+++ isPalindromee.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.Collections;
+
+public class isPalindromee {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int rev = 0;
+        int check = x;
+        while (check != 0) {
+            int temp = check % 10;
+            rev = rev * 10 + temp;
+            check /= 10;
+        }
+        return x == rev;
+        }
+    }
